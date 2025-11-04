3d5f05ac432005012a9ceb205114f1b65930f83d
konstantinfadin <fadin.constantine@gmail.com>
2025-11-04 09:08:33 +0300

    feat: created dtos and repository

--- userservice/src/main/java/com/example/userservice/dto/AuthRequest.java
+++ userservice/src/main/java/com/example/userservice/dto/AuthRequest.java
@@ -1,4 +1,13 @@
 package com.example.userservice.dto;
 
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
 public class AuthRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
---
+++ userservice/src/main/java/com/example/userservice/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.example.userservice.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+public class RegisterRequest {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    private String phone;
+    private String fullName;
+}
