500562a04c81d6f72b0d2e5be509f372aff1541e
konstantinfadin <fadin.constantine@gmail.com>
2025-11-05 08:59:12 +0300

    feat: added classes for connect

---
+++ userservice/src/main/java/com/example/userservice/dto/PendingBank.java
@@ -0,0 +1,19 @@
+package com.example.userservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class PendingBank {
+    private String bankCode;
+    private String bankName;
+    private String requestId;
+    private String message;
+    private String actionRequired;
+}
