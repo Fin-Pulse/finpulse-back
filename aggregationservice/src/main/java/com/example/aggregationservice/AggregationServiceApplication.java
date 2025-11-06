f21208b883c300e649b02d81b3b58859a83d03b2
konstantinfadin <fadin.constantine@gmail.com>
2025-11-06 08:30:06 +0300

    feat: added classes for connect

--- aggregationservice/src/main/java/com/example/aggregationservice/AggregationServiceApplication.java
+++ aggregationservice/src/main/java/com/example/aggregationservice/AggregationServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.aggregationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class AggregationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AggregationServiceApplication.class, args);
